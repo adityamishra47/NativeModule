d24505d80235079f1eab012ea23d74059fc0a272
adityamishra47 <am.dec14@gmail.com>
2024-10-11 01:57:20 +0530

    Initial commit of native module

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  NativeModule
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <View>
+        <Text
+          onPress={() => { }}
+          style={styles.txtBtn}>Open Login Page</Text>
+        <Text
+          onPress={() => { }}
+          style={styles.txtBtn}>Open Signup Page</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  txtBtn: {
+    padding: 14,
+    margin: 14,
+    borderWidth: 1,
+    backgroundColor: "dodgerblue",
+    color: "white",
+    fontSize: 16
+  }
+});
+
+export default App;
